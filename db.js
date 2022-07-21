ce79bb0741eb55c2f396b5ac09045429ed582373
Barchynai Kochkonbai kyzy <barchynai_kochkonbai_kyzy@epam.com>
2022-07-21 19:00:58 +0600

    added

--- db.js
+++ db.js
@@ -1,36 +1,12 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const courses = require("./data/courses.json");
+const auth = require("./data/auth.json");
+const topRated = require("./data/topRated.json")
+
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes
+  courses,
+  auth,
+  topRated
 }
 
 
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
- 
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
